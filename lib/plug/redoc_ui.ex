85c8a4e4f94b153eef84cfc72a3bfb5fdd9d9ec0
Gonzalo R <456459+grzuy@users.noreply.github.com>
2026-06-22 20:50:27 -0300

    fix(html): fixes title tag closing (#15)

--- lib/plug/redoc_ui.ex
+++ lib/plug/redoc_ui.ex
@@ -28,7 +28,7 @@ defmodule Redoc.Plug.RedocUI do
   <!doctype html>
   <html>
     <head>
-      <title><%= title %></title
+      <title><%= title %></title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
